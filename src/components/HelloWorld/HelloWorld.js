00445923b7507194032cc4a5c4942c4225c3e6e5
DESKTOP-9E52QIM\thanhpro <duydat246@gmail.com>
2022-05-06 00:07:25 +0700

    hello-world

---
+++ src/components/HelloWorld/HelloWorld.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const HelloWorld = () => {
+    const formatName = () => {
+        return user.firstName + ' ' + user.lastName;
+    }
+
+    const user = {
+        firstName: 'John',
+        lastName: 'Doe',
+    }
+    return (
+        <div className="hello-world">
+            Hello, {formatName(user)}
+        </div>
+    )
+}
+
+export default HelloWorld
